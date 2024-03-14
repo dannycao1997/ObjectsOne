36368a0c40543eea4ca4672f75b952be5b504aed
Danny Cao <dannycao1997@yahoo.com>
2024-03-13 22:30:55 -0400

    updating comments / finished

--- object1/src/main/java/object1/Person.java
+++ object1/src/main/java/object1/Person.java
@@ -4,10 +4,10 @@ class Person {
     // Define Instance Variables
     // different for each object!
     String name;
-    int age = 0;
-    int height = 0; //centimeters
+    int age;
+    int height; //centimeters
 
-    // Constructor
+    // Constructor and initialing instance variables
     public Person(String name, int age, int height) {
         this.name = name;
         this.age = age;
